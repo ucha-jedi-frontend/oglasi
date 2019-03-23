d96bd1ebca71f4ef68ba29cc87e0a494a4bb2137
jelena <lukavacj@gmail.com>
2019-03-23 12:37:46 +0100

    uskladjene promene

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,3 +1,67 @@
+// /*MODALI*/
+// $( '#btn-prijava' ).click(function() {
+//   $('#modal-prijava').addClass('opened');
+//   $('body').addClass('non-scroll');
+// });
+
+// $( '#btn-registracija' ).click(function() {
+// 	openregistracija();
+// });
+
+// // dugme na modalu prijava
+// $( '#dugme-registracija1' ).click(function() {
+// 	$('#modal-prijava').removeClass('opened');
+// 	openregistracija();
+// });
+
+
+// function openregistracija() {
+//   $('#modal-registracija').addClass('opened');
+//   $('body').addClass('non-scroll');
+// }
+
+// $( '.close-modal' ).click(function() {
+//   $(this).parent().parent().removeClass('opened');
+//   $('body').removeClass('non-scroll');
+// });
+
+
+// // const _api = axios.create({
+// //   baseURL:`http://localhost:3000`
+// // });
+
+
+// $('#dugme-login1').click(async()=>{
+//   const user = $('#email-prijava').val();
+//   const pass = $('#lozinka-prijava').val();
+//   let response = await _api.get(`/users?email=${user}&password=${pass}`);
+//   let users = await response.data;
+//   if(users[0]=== undefined){
+//     console.log('prazan')
+//   } else  {
+//     console.log('ima korisnika')
+//   }
+// })
+
+// $('#dugme-registracija').click(()=>{
+//   const mail = $('#email-registracija').val();
+//   const pass1 = $('#lozinka-registracija1').val();
+//   const pass2 = $('#lozinka-registracija2').val();
+//   if(pass1 === pass2){
+//    _api.post('/users', {
+//         email: mail,
+//         password: pass1
+//       })
+//       .catch(function (error) {
+//         console.log(error);
+//       });
+//     }else{
+//       alert('Ponovljena lozinka nije odgovarajuca')
+//     }
+//   })
+
+
+
 /*MODALI*/
 $( '#btn-prijava' ).click(function() {
   $('#modal-prijava').addClass('opened');
@@ -26,22 +90,44 @@ $( '.close-modal' ).click(function() {
 });
 
 
-// const _api = axios.create({
-//   baseURL:`http://localhost:3000`
-// });
+
+$( '#dugme-login1' ).click(function() {
+             var accessGranted = false;
+             checkUser($('#email-prijava').val(),$('#lozinka-prijava').val()).then(function(result) {
+   					accessGranted = result;
+                  if(accessGranted){
+                  	//$('#btn-registracija').addClass('hidden');
+                   	$('#btn-prijava').addClass('hidden');
+                     $('#btn-registracija').html('Uspesno ste se ulogovali').attr('disabled','true');                 	
+                  	$('.crta').addClass('hidden');
+                  	$('#modal-prijava').removeClass('opened');
+                  	$('body').removeClass('non-scroll');		                 
+                  }else {
+                  alert('Pogrešna e-mail adresa ili lozinka. Pokušajte ponovo.'); 	
+                 }    				
+             });
+});
 
 
-$('#dugme-login1').click(async()=>{
-  const user = $('#email-prijava').val();
-  const pass = $('#lozinka-prijava').val();
-  let response = await _api.get(`/users?email=${user}&password=${pass}`);
-  let users = await response.data;
-  if(users[0]=== undefined){
-    console.log('prazan')
-  } else  {
-    console.log('ima korisnika')
-  }
-})
+ async function checkUser(in_email,in_password){
+                let response = await _api.get(`/users`);
+                let users = await response.data; 
+                for (const user of users) {
+                  let userId = user.id;
+                  let userpass = user.password;
+                  let email = user.email;
+                  if (in_email==email && in_password==userpass) {
+                  	return true;
+                  } 
+                 
+                }
+                return false;
+              }
+              
+  $('.nosubmit').submit(function (evt) {
+    evt.preventDefault();
+});            
+
 
 $('#dugme-registracija').click(()=>{
   const mail = $('#email-registracija').val();
@@ -55,6 +141,10 @@ $('#dugme-registracija').click(()=>{
       .catch(function (error) {
         console.log(error);
       });
+      $('#btn-registracija').html('Uspesno ste se registrovali. Mozete da se ulogujete').attr('disabled','true'); 
+      $('#modal-registracija').removeClass('opened');
+      $('body').removeClass('non-scroll');	
+      
     }else{
       alert('Ponovljena lozinka nije odgovarajuca')
     }
